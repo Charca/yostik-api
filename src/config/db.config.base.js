c8694cf6136e7f4144ce2db7dc7d480acf26f3d5
Maxi Ferreira <charca@gmail.com>
2016-04-27 11:58:56 -0700

    Ignored db.config.js file

---
+++ src/config/db.config.base.js
@@ -0,0 +1,11 @@
+// Create db.config.js with this configuration
+module.exports = {
+  client: 'mysql',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'yostik',
+    charset  : 'utf8'
+  }
+};
