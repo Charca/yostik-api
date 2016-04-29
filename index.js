0a172b5466955d7ccb77da530dae98155618a40c
Maxi Ferreira <charca@gmail.com>
2016-04-29 16:02:01 -0700

    Added logmessage endpoint

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Hapi = require('hapi');
 const Joi = require('joi');
 const Game = require('./src/models/Game');
 const Deal = require('./src/models/Deal');
+const Message = require('./src/models/Message');
 
 const server = new Hapi.Server();
 server.connection({
@@ -49,6 +50,43 @@ server.route({
     }
   }
 });
+
+server.route({
+  method: 'POST',
+  path: '/api/v1/logmessage',
+  handler: function(request, reply) {
+    const data = {
+      messenger_platform_id: request.payload.messenger_platform_id,
+      external_message_id: request.payload.external_message_id,
+      external_user_id: request.payload.external_user_id,
+      type: request.payload.type,
+      text: request.payload.text
+    };
+
+    const message = new Message(data).save();
+    message.then((message) => {
+      reply({
+        success: true,
+        message_id: message.id
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      reply(err);
+    });
+  },
+  config: {
+    validate: {
+      payload: Joi.object({
+        messenger_platform_id: Joi.number().required(),
+        external_message_id: Joi.string().required(),
+        external_user_id: Joi.number().required(),
+        type: Joi.string().required(),
+        text: Joi.string().required()
+      })
+    }
+  }
+})
 
 server.route({
   method: 'GET',
