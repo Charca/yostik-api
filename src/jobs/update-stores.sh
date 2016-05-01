1430e876a2d18138c410836e3eb8b1130f4094c7
Maxi Ferreira <charca@gmail.com>
2016-05-01 12:38:55 -0700

    Updated update-stores job

--- src/jobs/update-stores.sh
+++ src/jobs/update-stores.sh
@@ -4,18 +4,18 @@ echo "Started running crawlers\n"
 echo "Remember you need to run this script from the jobs/ dir"
 
 echo "Job: Running Amazon Crawler"
-node ../stores/Amazon.js
+/usr/local/bin/node ../stores/Amazon.js
 
 echo "Job: Running Steam Crawler"
-node ../stores/CheapShark.js steam
+/usr/local/bin/node ../stores/CheapShark.js steam
 
 echo "Job: Running WinGameStore Crawler"
-node ../stores/CheapShark.js wingamestore
+/usr/local/bin/node ../stores/CheapShark.js wingamestore
 
 echo "Job: Running Gog Crawler"
-node ../stores/Gog.js
+/usr/local/bin/node ../stores/Gog.js
 
 echo "Job: Running PlayStationStore Crawler"
-node ../stores/PlayStationStore.js
+/usr/local/bin/node ../stores/PlayStationStore.js
 
 echo "Finished running crawlers!"
